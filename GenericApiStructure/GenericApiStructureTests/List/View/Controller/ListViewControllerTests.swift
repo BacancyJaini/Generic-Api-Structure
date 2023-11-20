5305b3eb10a06395690b30fc1b9e5392dc4afe6e
Jaini <jaini.darji@bacancy.com>
2023-11-20 17:21:25 +0530

    test case folder added

---
+++ GenericApiStructure/GenericApiStructureTests/List/View/Controller/ListViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewControllerTests.swift
+//  GenericApiStructureTests
+//
+//  Created by user on 20/11/23.
+//
+
+import UIKit
+@testable import GenericApiStructure
+import XCTest
+
+class ListViewControllerTests: XCTestCase {
+
+}
